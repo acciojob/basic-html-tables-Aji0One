57a36c8eab33a89c569890f0ed69dcc81e092cc2
Aji0One <109173463+Aji0One@users.noreply.github.com>
2025-06-09 23:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const tbody=document.getElementById("tbody");
+
+
+const values=[{num:332432,name:"fdsf"},{num:4322,name:"knfkf"}];
+values.forEach((ele,ind)=>{
+	const trow=document.createElement("tr");
+	trow.className="trow";
+	const tdnum=document.createElement("td");
+	const tdname=document.createElement("td");
+	tdnum.innerText=ele.num;
+	tdname.innerText=ele.name;
+	trow.appendChild(tdnum);
+	trow.appendChild(tdname);
+
+	tbody.append(trow);
+})
